08b347f53727269713a53731931e76a9cc28ee78
hojatv <hojat@Hojatollahs-MBP.fritz.box>
2020-09-14 18:18:57 +0200

    Repository Model

--- src/main/java/com/islamversity/exception/DataNotFoundException.java
+++ src/main/java/com/islamversity/exception/DataNotFoundException.java
@@ -3,6 +3,6 @@ package com.islamversity.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Booking not found!")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Data not found!")
 public class DataNotFoundException extends RuntimeException{
 }
--- src/main/java/com/islamversity/service/repository/SurahRepository.java
+++ src/main/java/com/islamversity/service/repository/SurahRepository.java
@@ -2,7 +2,11 @@ package com.islamversity.service.repository;
 
 import com.islamversity.service.domain.Surah;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface SurahRepository extends CrudRepository<Surah, Long> {
-
+    Optional<Surah> findById(Long surahId);
 }
